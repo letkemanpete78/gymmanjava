3b4f6b88478bd2190a9aae92b05a20b0f515bbcc
peteletkeman78 <letkemanpete@gmail.com>
2020-08-18 15:35:41 -0400

    routine test updates

--- src/test/java/ca/letkeman/gymmanjava/service/RoutineServiceTest.java
+++ src/test/java/ca/letkeman/gymmanjava/service/RoutineServiceTest.java
@@ -1,19 +1,15 @@
 package ca.letkeman.gymmanjava.service;
 
+import static ca.letkeman.gymmanjava.service.Util.setActivityValues;
+import static ca.letkeman.gymmanjava.service.Util.setExerciseValues;
+import static ca.letkeman.gymmanjava.service.Util.setRoutineValues;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
-import ca.letkeman.gymmanjava.dao.ActivityRepository;
-import ca.letkeman.gymmanjava.dao.ResourceFileRepository;
 import ca.letkeman.gymmanjava.dao.RoutineRepository;
-import ca.letkeman.gymmanjava.models.Activity;
 import ca.letkeman.gymmanjava.models.Exercise;
-import ca.letkeman.gymmanjava.models.ResourceFile;
 import ca.letkeman.gymmanjava.models.Routine;
-import ca.letkeman.gymmanjava.models.Type;
-import ca.letkeman.gymmanjava.service.interfaces.StorageService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -35,9 +31,10 @@ class RoutineServiceTest {
   private RoutineService routineService;
 
   @Test
-  void delete() {
+  void deleteShouldBeCalled() {
     List<Routine> routines = initList(2);
-    when(routineRepository.findAllByuuidIn(Collections.singletonList("1 routine test-uuid"))).thenReturn(routines);
+    when(routineRepository.findAllByuuidIn(Collections.singletonList("1 routine test-uuid")))
+        .thenReturn(routines);
     doNothing().when(routineRepository).delete(routines.get(0));
     boolean wasDeleted = routineService.delete("1 routine test-uuid");
     Assertions.assertTrue(wasDeleted);
@@ -68,7 +65,7 @@ class RoutineServiceTest {
     Routine routine = initList(1).get(0);
     when(routineRepository.findByuuid("1 routine test-uuid")).thenReturn(routine);
     Routine routine1 = routineService.get("1 routine test-uuid");
-    Assertions.assertEquals(routine,routine1);
+    Assertions.assertEquals(routine, routine1);
   }
 
   @Test
@@ -76,49 +73,18 @@ class RoutineServiceTest {
     List<Routine> routines = initList(3);
     when(routineRepository.findAll()).thenReturn(routines);
     List<Routine> routineList = routineService.list();
-    Assertions.assertEquals(routineList,routines);
+    Assertions.assertEquals(routineList, routines);
   }
 
 
-  private List<Routine> initList(int size){
+  private List<Routine> initList(int size) {
     List<Routine> routines = new ArrayList<>();
-    for (int j = 1;j<=size;j++){
+    for (int j = 1; j <= size; j++) {
       List<Exercise> exercises = new ArrayList<>();
-
       for (int i = 1; i <= size; i++) {
-
-        ResourceFile resourceFile = new ResourceFile();
-        resourceFile.setDescription(i + " test description");
-        resourceFile.setFileName(i + " test-file.jpg");
-        resourceFile.setFileSize(123 * i);
-        resourceFile.setDateTime(LocalDateTime.now());
-        resourceFile.setFileId(i);
-
-        Activity activity = new Activity();
-        activity.setDescription(i + " activity test description");
-        activity.setName(i + " activity test name");
-        activity.setResourceFile(resourceFile);
-        activity.setUuid(i + " activity test-uuid");
-        activity.setId(i);
-
-        Exercise exercise = new Exercise();
-        exercise.setActivity(activity);
-        exercise.setUuid(i + " exercise test-uuid");
-        exercise.setDescription(i + " exercise test description");
-        exercise.setId(i);
-        exercise.setName(i + " exercise name");
-        exercise.setType(Type.TIMED);
-        exercise.setValue(10);
-        exercise.setUnit("seconds");
-        exercises.add(exercise);
+        exercises.add(setExerciseValues(i, setActivityValues(i)));
       }
-      Routine routine = new Routine();
-      routine.setExercises(exercises);
-      routine.setDescription(j + " routine test description");
-      routine.setId(j);
-      routine.setName(j + " routine test name");
-      routine.setUuid(j + " routine test-uuid");
-      routines.add(routine);
+      routines.add(setRoutineValues(j, exercises));
     }
     return routines;
   }
